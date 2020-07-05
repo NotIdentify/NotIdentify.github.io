d7180a331abc02aa814867f0d0aafc0b91d519e9
Dipkill <tolol@protonmail.com>
2020-07-04 23:08:41 -0700

    commit

---
+++ assets/js/content.js
@@ -0,0 +1,43 @@
+var htmlasli = `
+<div class="col-md-4">
+	<div class="card mb-4 box-shadow bg-dark shadow">
+	<img class="card-img-top" src="$img$" alt="Card image cap">
+	<div class="card-body">
+		<h5>$title$</h5>
+		<p class="card-text text-mute">
+			$text$
+		</p>
+		<div class="d-flex justify-content-between align-items-center">
+		<div class="btn-group">
+			<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+		</div>
+		<small class="text-muted">9 mins</small>
+		</div>
+	</div>
+	</div>
+</div>`;
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "../content/content.txt", true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var myArr = JSON.parse(this.responseText);
+		// html = html.replace("$img$", myArr[0].img)
+		// html = html.replace("$title$", myArr[0].title)
+		// html = html.replace("$text$", myArr[0].spoiler)
+		var arrayContent = [];
+		for(var i=0; i<myArr.length; i++){
+			html = htmlasli.replace("$img$", myArr[i].img)
+			html = html.replace("$title$", myArr[i].title)
+			html = html.replace("$text$", myArr[i].spoiler)
+			arrayContent.push(html)
+		}
+		// document.getElementById("album").innerHTML = html;
+		var isi = "";
+		for (var i=0; i<arrayContent.length; i++){
+			isi += arrayContent[i];
+		}
+		document.getElementById("album").innerHTML = isi
+		
+	}
+};
