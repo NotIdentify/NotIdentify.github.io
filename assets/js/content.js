dcf6067165b2a8d3553c14268546869beb72c597
Dipkill <tolol@protonmail.com>
2020-07-07 21:01:42 -0700

    commit

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,21 +1,34 @@
+// var htmlasli1 = `
+// <div class="col-md-4">
+// 	<div class="card mb-4 box-shadow bg-dark shadow">
+// 	<img class="card-img-top" src="$img$" alt="Card image cap">
+// 	<div class="card-body">
+// 		<h5>$title$</h5>
+// 		<p class="card-text text-mute">
+// 			$text$
+// 		</p>
+// 		<div class="d-flex justify-content-between align-items-center">
+// 		<div class="btn-group">
+// 			<a href="$url$" type="button" class="btn btn-sm btn-outline-secondary">View</a>
+// 		</div>
+// 		<small class="text-muted">9 mins</small>
+// 		</div>
+// 	</div>
+// 	</div>
+// </div>`;
+
 var htmlasli = `
-<div class="col-md-4">
-	<div class="card mb-4 box-shadow bg-dark shadow">
-	<img class="card-img-top" src="$img$" alt="Card image cap">
-	<div class="card-body">
-		<h5>$title$</h5>
-		<p class="card-text text-mute">
-			$text$
-		</p>
-		<div class="d-flex justify-content-between align-items-center">
-		<div class="btn-group">
-			<a href="$url$" type="button" class="btn btn-sm btn-outline-secondary">View</a>
+<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+	<div class="hovereffect shadow-own rounded" style="background: linear-gradient(to right, #414345, #232526);">
+        <img class="img-responsive" src="$img$" alt="">
+        <div class="overlay">
+           <h2>$title$</h2>
+           <a class="info" href="$url$" target="_blank">View More</a>
 		</div>
-		<small class="text-muted">9 mins</small>
-		</div>
-	</div>
-	</div>
-</div>`;
+		<div class="container bg-green text-center  p-2 ">HOVER ME!</div>
+    </div>
+</div>
+`
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "../content/content.txt", true);
 xmlhttp.send();
@@ -29,7 +42,7 @@ xmlhttp.onreadystatechange = function() {
 		for(var i=0; i<myArr.length; i++){
 			html = htmlasli.replace("$img$", myArr[i].img)
 			html = html.replace("$title$", myArr[i].title)
-			html = html.replace("$text$", myArr[i].spoiler)
+			// html = html.replace("$text$", myArr[i].spoiler)
 			html = html.replace("$url$", myArr[i].url)
 			arrayContent.push(html)
 		}
